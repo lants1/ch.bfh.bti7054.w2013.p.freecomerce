3717a2e1e26589306b6209a51e2009be8a64922d
lants1 <swen.lanthemann@gmail.com>
2013-12-31 16:16:43 +0100

    Task 4.2

---
+++ freecomerce/php/nav.php
@@ -0,0 +1,12 @@
+<?php 
+
+// Links Array gemäss Aufgabe 4.2 erstellen und ausgeben...
+$links = array("<a href='html/blacktea.html'>Schwarztee</a>","<a href='html/whitetea.html'>Weisstee</a>","<a href='html/greentea.html'>Grüntee</a>",
+"<a href='html/bluetea.html'>Blautee</a>");
+echo "<ul>";
+foreach($links AS $link)
+{
+	echo "<li>".$link."</li>";
+}
+echo "<ul>";
+?>

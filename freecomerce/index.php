fa5fa75187779ccb06ba39d3bde081a846247f63
lants1 <swen.lanthemann@gmail.com>
2013-12-31 14:13:15 +0100

    Task 3.2

--- freecomerce/index.php
+++ freecomerce/index.php
@@ -1,3 +1,43 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Zateeo Webshop</title>
+<link rel="stylesheet" type="text/css" href="css/layout.css" />
+</head>
+<body>
+	<div class="content">
+		<div class="top_block header">
+			<div class="content">
+			<img src="gfx/zateeo_logo.png" alt="Zateeo Logo" class="left"/>
+			<div style="float:right;">
+				<a href="shoppingchart.html"><img src="gfx/cart.png" alt="Warenkorb"/></a>
+			</div>
+			</div>
+		</div>
+		<div class="background navigation">
+		</div>
+		<div class="left_block navigation">
+			<div class="content">
+			Navigation
+			<?php include("php/nav.php"); ?>
+			</div>
+		</div>
+		<div class="maincontent">
+			Inhalt
+			<?php include("php/content.php"); ?>
+		</div>
+		<div class="bottom_block_adv advertisement">
+			<div class="content">
+			Advertisement
+			<?php include("php/adv.php"); ?>
+			</div>
+		</div>
+		<div class="bottom_block footer">
+			<div class="content">
+			Footer
+			</div>
+		</div>
+	</div>
+</body>
+</html>

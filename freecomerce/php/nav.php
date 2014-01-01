91e99423d5e61925f689158c6ca198771260089c
lants1 <swen.lanthemann@gmail.com>
2014-01-01 11:57:23 +0100

    Task 5.1, Task 5.2

--- freecomerce/php/nav.php
+++ freecomerce/php/nav.php
@@ -1,12 +1,29 @@
-<?php 
+<?php
 
-// Links Array gemäss Aufgabe 4.2 erstellen und ausgeben...
-$links = array("<a href='html/blacktea.html?cat_id=1'>Schwarztee</a>","<a href='html/whitetea.html'>Weisstee</a>","<a href='html/greentea.html'>Grüntee</a>",
-"<a href='html/bluetea.html'>Blautee</a>");
+// Task 5.2 Gebe jeder Seite eine ID...
+$links [0] ['cat_id'] = 1;
+$links [0] ['link'] = "?cat_id=1'>Schwarztee</a></div></li>";
+
+$links [1] ['cat_id'] = 2;
+$links [1] ['link'] = "?cat_id=2'>Weisstee</a></div></li>";
+
+$links [2] ['cat_id'] = 3;
+$links [2] ['link'] = "?cat_id=3'>Gr&uuml;ntee</a></div></li>";
+
+$links [3] ['cat_id'] = 4;
+$links [3] ['link'] = "?cat_id=4'>Blautee</a></div></li>";
+
+// Task 5.1 ändere Design des Links für die aktuelle Seite
+// via PHP und CSS...
 echo "<ul>";
-foreach($links AS $link)
-{
-	echo "<li>".$link."</li>";
-}
-echo "<ul>";
+// Listenüberischt der Kategorie
+foreach ( $links as $link ) {
+	if (isset ( $_GET ['cat_id']) && $link['cat_id'] == $_GET['cat_id'] ) {
+			echo "<li><div class='current-nav-page'><a href='".$_SERVER['PHP_SELF'].$link['link'];
+	} else {
+			echo "<li><div class='nav-page'><a href='".$_SERVER['PHP_SELF'].$link['link'];
+		}
+	}
+echo "</ul>";
+
 ?>

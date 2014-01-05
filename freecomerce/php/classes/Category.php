43ae5460262f937bfdfc18a9c3d2556c607f4d4f
lants1 <swen.lanthemann@gmail.com>
2014-01-05 09:27:56 +0100

    mysqli integration for task 10.3, 10.4 and 9.2...

--- freecomerce/php/classes/Category.php
+++ freecomerce/php/classes/Category.php
@@ -1,16 +1,81 @@
 <?php
-	class Category
-	{
-		private $cat_id = 0;
-		private $text_de = "";
-		private $text_en = "";
-		
-		//Konstruktor
-		public function __construct($prod_id, $text_de, $text_en)
-		{
-			$this->cat_Id = $prod_id;
-			$this->text_de = $text_de;
-			$this->text_en = $text_en;
-		}
+class Category {
+	private $cat_id = 0;
+	private $text_de = "";
+	private $text_en = "";
+	
+	// Konstruktor
+	public function __construct($cat_id, $text_de, $text_en) {
+		$this->cat_Id = $cat_id;
+		$this->text_de = $text_de;
+		$this->text_en = $text_en;
 	}
+	
+	/**
+	 * Insert Category into DB...
+	 *
+	 * @param unknown $cat_id        	
+	 * @param unknown $text_de        	
+	 * @param unknown $text_en        	
+	 */
+	public function insertCategory($cat_id, $text_de, $text_en) {
+		$query = "INSERT INTO categorie (cat_id, text_de, text_en) VALUES ('$cat_id', '$text_de', '$text_en')";
+		$this->query ( $query );
+	}
+	
+	/**
+	 * Update Category...
+	 *
+	 * @param unknown $cat_id
+	 * @param unknown $text_de
+	 * @param unknown $text_en
+	 */
+	public function updateCategory($cat_id, $text_de, $text_en) {
+		$query = "UPDATE categorie SET text_de='$text_de', text_en='$text_en' where cat_id = '$cat_id'";
+		$this->query ( $query );
+	}
+	
+	/**
+	 * Get all categories
+	 *
+	 * @param unknown $cat_id        	
+	 * @return mixed
+	 */
+	public function getCategories() {
+		return $this->query ( "SELECT * FROM categorie");
+	}
+	
+	/**
+	 * Delete Category by id...
+	 *
+	 * @param unknown $id        	
+	 */
+	public function deleteCategory($id) {
+		$this->query ( "DELETE FROM categorie WHERE cat_id = $id" );
+	}
+	
+	/**
+	 * Get Category by id
+	 *
+	 * @param unknown $id        	
+	 * @return mixed
+	 */
+	public function getCategory($id) {
+		return $this->query ( "SELECT * FROM categorie WHERE cat_id = $id" );
+	}
+	
+	/**
+	 * create a new db connection
+	 */
+	function __construct() {
+		parent::__construct ( "localhost", "demo1", "demo1", "freecomerce" );
+	}
+	
+	/**
+	 * end db connection
+	 */
+	function __destruct() {
+		parent::close ();
+	}
+}
 ?>
--- freecomerce/php/classes/Product.php
+++ freecomerce/php/classes/Product.php
@@ -1,5 +1,5 @@
 <?php
-	class Product
+	class Product extends mysqli
 	{
 		private $prod_id = 0;
 		private $text_de = "";
@@ -17,6 +17,81 @@
 			$this->img_path = $img_path;
 			$this->price = $price;
 			$this->cat_id = $cat_id;
+		}
+		
+		/**
+		 * Insert a Product into DB.
+		 * 
+		 * @param unknown $prod_id
+		 * @param unknown $text_de
+		 * @param unknown $text_en
+		 * @param unknown $img_path
+		 * @param unknown $price
+		 * @param unknown $cat_id
+		 */
+		public function insertProduct($prod_id, $text_de, $text_en, $img_path, $price, $cat_id) {
+			$query = "INSERT INTO product (prod_id, text_de, text_en, img_path, price, cat_id) VALUES ('$prod_id', '$text_de', '$text_en', '$img_path', '$price', '$cat_id')";
+			$this->query ( $query );
+		}
+		
+		/**
+		 * Update Category...
+		 *
+		 * @param unknown $cat_id
+		 * @param unknown $text_de
+		 * @param unknown $text_en
+		 */
+		public function updateProduct($prod_id, $text_de, $text_en, $img_path, $price) {
+			$query = "UPDATE product SET text_de='$text_de', text_en='$text_en', img_path='$img_path', price='$price' where prod_id = '$prod_id'";
+			$this->query ( $query );
+		}
+		
+		/**
+		 * Get a list of products by category
+		 * 
+		 * @param unknown $cat_id
+		 * @return mixed
+		 */
+		public function getProductsByCategory($cat_id)
+		{
+			return $this->query("SELECT * FROM product WHERE prod_cat_id = $cat_id");
+		}
+		
+		/**
+		 * Delete Product by id...
+		 * 
+		 * @param unknown $id
+		 */
+		public function deleteProduct($id)
+		{
+			$this->query("DELETE FROM product WHERE prod_id = $id");
+		}
+		
+		/**
+		 * Get Product by id
+		 * 
+		 * @param unknown $id
+		 * @return mixed
+		 */
+		public function getProduct($id)
+		{
+			return $this->query("SELECT * FROM product WHERE prod_id = $id");
+		}
+		
+		
+		
+		/**
+		 * create a new db connection
+		 */
+		function __construct() {
+			parent::__construct ( "localhost", "demo1", "demo1", "freecomerce" );
+		}
+		
+		/**
+		 * end db connection
+		 */
+		function __destruct() {
+			parent::close ();
 		}
 	}
 ?>

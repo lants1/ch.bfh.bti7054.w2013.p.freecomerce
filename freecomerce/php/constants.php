f01d8573d8bcef7662645b0c1976b4736af308e8
lants1 <swen.lanthemann@gmail.com>
2014-01-17 07:48:25 +0100

    demodata added

--- freecomerce/php/constants.php
+++ freecomerce/php/constants.php
@@ -29,4 +29,38 @@ $products [2] ['herkunft'] = "Sri-Lanka";
 $products [2] ['preis'] = 2.40;
 $products [2] ['img'] = "gfx/pai-mu-tan.jpg";
 $products [2] ['text'] = "jklaljkadsfljadsfljkadsfljkadsljdasadsffadsljkadsfljkfsaölksfdjaklösdfjölkasfdjölkajfölkasjfdlköasjflköajslkfj";
+
+
+$products [3] ['product_id'] = 4;
+$products [3] ['name'] = "Oolong-fujian";
+$products [3] ['cat_id'] = 2;
+$products [3] ['herkunft'] = "Japan";
+$products [3] ['preis'] = 9.10;
+$products [3] ['img'] = "gfx/oolong-fujian.jpg";
+$products [3] ['text'] = "jklaljkadsfljadsfljkadsfljkadsljdasadsffadsljkadsfljkfsaölksfdjaklösdfjölkasfdjölkajfölkasjfdlköasjflköajslkfj";
+
+$products [4] ['product_id'] = 5;
+$products [4] ['name'] = "Sencha-shizuoka";
+$products [4] ['cat_id'] = 3;
+$products [4] ['herkunft'] = "Pakistan";
+$products [4] ['preis'] = 15.20;
+$products [4] ['img'] = "gfx/sencha-shizuoka.jpg";
+$products [4] ['text'] = "jklaljkadsfljadsfljkadsfljkadsljdasadsffadsljkadsfljkfsaölksfdjaklösdfjölkasfdjölkajfölkasjfdlköasjflköajslkfj";
+
+$products [5] ['product_id'] = 6;
+$products [5] ['name'] = "Ceylon-nuwara";
+$products [5] ['cat_id'] = 4;
+$products [5] ['herkunft'] = "Indien";
+$products [5] ['preis'] = 973.20;
+$products [5] ['img'] = "gfx/ceylon-nuwara.jpg";
+$products [5] ['text'] = "jklaljkadsfljadsfljkadsfljkadsljdasadsffadsljkadsfljkfsaölksfdjaklösdfjölkasfdjölkajfölkasjfdlköasjflköajslkfj";
+
+$products [6] ['product_id'] = 7;
+$products [6] ['name'] = "Noir d'Orange";
+$products [6] ['cat_id'] = 4;
+$products [6] ['herkunft'] = "Japan";
+$products [6] ['preis'] = 973.20;
+$products [6] ['img'] = "gfx/ceylon-nuwara.jpg";
+$products [6] ['text'] = "jklaljk'adsfljadsfljkadsfljkadsljdasadsffadsljkadsfljkfsaölksfdjaklösdfjölkasfdjölkajfölkasjfdlköasjflköajslkfj";
+
 ?>
